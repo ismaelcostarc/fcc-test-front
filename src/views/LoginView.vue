048dcfc04fda91a34bda4857265e7b60ba0ff067
Ismael Costa <ismaelac96@icloud.com>
2024-05-03 14:15:03 -0300

    feat: creating the first view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <form>
+      <label>
+        E-mail:
+        <input type="text" />
+      </label>
+
+      <label>
+        Senha:
+        <input type="password" />
+      </label>
+
+      <button type="submit">Entrar</button>
+    </form>
+  </main>
+</template>
