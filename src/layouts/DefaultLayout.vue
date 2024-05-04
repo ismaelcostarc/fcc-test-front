fab230df4b6dc7ec7dfbc062fcc0450e4654c2a1
Ismael Costa <ismaelac96@icloud.com>
2024-05-04 17:52:15 -0300

    feat: create header of default layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,5 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue'
+
+const logout = () => {}
+</script>
 <template>
-  default
-  <div><slot></slot></div>
+  <header>
+    <h1>Sistema de gerenciamento de clientes</h1>
+    <BaseButton @click="logout">Sair</BaseButton>
+  </header>
+  <main><slot></slot></main>
+  <footer></footer>
 </template>
+
+<style scoped>
+header {
+  height: 20vh;
+  background-color: var(--color-primary);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2em;
+}
+
+h1 {
+  color: var(--color-white);
+  font-weight: bold;
+  font-size: var(--font-size-xl);
+}
+
+main {
+}
+footer {
+}
+</style>
