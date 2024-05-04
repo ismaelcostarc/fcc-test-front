9abd4a2b879f48256388fa9b9737acc2748c0cb0
Ismael Costa <ismaelac96@icloud.com>
2024-05-04 18:06:27 -0300

    feat: create default layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,20 +1,34 @@
 <script setup lang="ts">
 import BaseButton from '@/components/BaseButton.vue'
+import router from '@/router'
 
-const logout = () => {}
+const logout = () => {
+  router.push({
+    name: ';ogin'
+  })
+}
 </script>
 <template>
   <header>
     <h1>Sistema de gerenciamento de clientes</h1>
     <BaseButton @click="logout">Sair</BaseButton>
   </header>
-  <main><slot></slot></main>
-  <footer></footer>
+  <main>
+    <div><slot></slot></div>
+  </main>
+  <footer>
+    <div>
+      Rua das Ideias, 123<br />
+      Bairro dos Sonhos<br />
+      São Paulo - SP, Brasil<br />
+      CEP: 01234-567
+    </div>
+  </footer>
 </template>
 
 <style scoped>
 header {
-  height: 20vh;
+  height: 10vh;
   background-color: var(--color-primary);
   display: flex;
   flex-direction: row;
@@ -30,7 +44,28 @@ h1 {
 }
 
 main {
+  background-color: var(--color-light-gray);
+  min-height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  padding: 2em 0;
 }
+
+main div {
+  background-color: var(--color-white);
+}
+
 footer {
+  height: 20vh;
+  background-color: var(--color-primary);
+  color: var(--color-white);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2em;
+  font-size: var(--font-size-sm);
+  line-height: var(--line-height-md);
 }
 </style>
