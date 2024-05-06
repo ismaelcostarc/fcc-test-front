d54444d85b5fb8f72488e5ede3f3b36a2a61099a
Ismael Costa <ismaelac96@icloud.com>
2024-05-05 21:17:58 -0300

    feat: add page not found

--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,2 +1,27 @@
-<script setup lang="ts"></script>
-<template><div></div></template>
+<script setup lang="ts">
+import BaseButton from '@/components/base/BaseButton.vue'
+import router from '@/router'
+
+const goHome = () => {
+  router.push({ name: 'customerList' })
+}
+</script>
+<template>
+  <div class="container">
+    <div class="message">Essa página não foi encontrada :(</div>
+    <BaseButton @click="goHome">Voltar ao início</BaseButton>
+  </div>
+</template>
+<style scoped>
+.message {
+  font-size: var(--font-size-xxl);
+  font-weight: 100;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  padding: 2em;
+}
+</style>
